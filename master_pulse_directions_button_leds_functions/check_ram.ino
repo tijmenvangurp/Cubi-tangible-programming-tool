48a4b8660706e1d8842ac90f868ee667be56746f
tijmenvangurp <tijmenvangurp@gmail.com>
2014-11-28 15:54:49 +0100

    Revert "Arduino project"
    This reverts commit 4f619384636c43a7f3b23798135e0e7cb010c797.

--- master_pulse_directions_button_leds_functions/check_ram.ino
+++
@@ -1,6 +0,0 @@
-int freeRam (String place) {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  Serial.print(place);
-  Serial.println( (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval)); 
-}
